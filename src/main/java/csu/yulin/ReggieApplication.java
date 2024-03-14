8b317ade1bd4137788b887e8b6e0cef9837192fc
刘飘 <2973288539@qq.com>
2024-03-14 20:13:44 +0800

    缓存套餐优化

--- src/main/java/csu/yulin/ReggieApplication.java
+++ src/main/java/csu/yulin/ReggieApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
@@ -12,7 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 //对 Servlet、Filter 和 Listener 的自动扫描和注册
 @ServletComponentScan
 @EnableTransactionManagement
-
+@EnableCaching
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
